8d779598d722d71ccc3f32cd605cbf0be9b9f436
Dirak44 <tommasohaas06@gmail.com>
2023-05-19 19:23:51 +0200

    In der VM Changes

---
+++ Powershell-Skript/adimport.ps1
@@ -0,0 +1,7 @@
+#--------------------------------------------------------------------------------
+# Autor: Tommaso und Rene
+# Funktion des Skripts: Das ist die Bulk-Funktion um die Benutzer zu impotieren.
+# Datum: 19.05.2023
+# Version: 1.0
+# Bemerkungen:
+#--------------------------------------------------------------------------------
---
+++ Powershell-Skript/anwendung.ps1
@@ -0,0 +1,8 @@
+#--------------------------------------------------------------------------------
+# Autor: Tommaso und Rene
+# Funktion des Skripts: Das ist unser Skript das wir ausführen und von dort auf die verschiede Funktion zuzugreifen
+# Datum: 19.05.2023
+# Version: 1.0
+# Bemerkungen:
+#--------------------------------------------------------------------------------
+
