5f30dc902011abfeb5e70c8b9327cb4d51586a54
Dirak44 <tommasohaas06@gmail.com>
2023-05-20 17:07:45 +0200

    Changes und Anpassung

---
+++ Powershell-Skript/test3 copy.ps1
@@ -0,0 +1,58 @@
+# Skript um AD Benutzer erstellen 
+
+Import-Module ".\Powershell-Skript\log.psm1"
+Import-Module ActiveDirectory
+$xml = [xml](Get-Content -Path "C:\pwsh\Los-Hermanos\schueler.xml")
+# Zählervariable initialisieren
+$counter = 0
+
+# Namespace definieren
+$namespace = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
+$namespace.AddNamespace("ns", "http://schemas.microsoft.com/powershell/2004/04")
+# foreach ($obj in $Global:schueler.SelectNodes("//ns:Obj", $namespace))
+#
+
+# Durch die Objekte in der XML-Datei iterieren
+foreach ($obj in $xml.SelectNodes("//ns:Obj", $namespace)) {
+    # MS-Element auslesen
+    $msElement = $obj.SelectSingleNode("ns:MS", $namespace).InnerText
+
+    # Attribute aus dem MS-Element extrahieren
+    $attributes = $msElement -split ';'
+
+    # Attribute in Variablen speichern
+    $name = $attributes[0]
+    $vorname = $attributes[1]
+    $benutzername = $attributes[2]
+    $klasse = $attributes[3]
+    $klasse2 = $attributes[4]
+
+    # Benutzer im AD erstellen
+    $schuelerParams = @{
+        'SamAccountName' = $benutzername
+        'GivenName' = $vorname
+        'Surname' = $name
+        'Name' = "$vorname $name"
+        'UserPrincipalName' = "$benutzername@butzbach.ch"
+        'Enabled' = $true
+        'PasswordNeverExpires' = $true
+        'AccountPassword' = (ConvertTo-SecureString -String 'DeinPasswort' -AsPlainText -Force)
+        'ChangePasswordAtLogon' = $false
+    }
+
+    #Erstellt die verschiedenen Klasse, wenn diese schon vorhanden sind.
+    
+
+    New-ADUser @schuelerParams -ErrorAction SilentlyContinue
+
+    # Benutzer der Klasse hinzufügen
+    Add-ADGroupMember -Identity $klasse -Members $benutzername -ErrorAction SilentlyContinue
+
+    # Benutzer der zweiten Klasse hinzufügen
+    Add-ADGroupMember -Identity $klasse2 -Members $benutzername -ErrorAction SilentlyContinue
+
+    # Fortschritt anzeigen
+    $counter++
+    Write-Host "Schueler erstellt: $benutzername" + "Anzahl impotierts: $counter"
+}
+
